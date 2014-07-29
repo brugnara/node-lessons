a37b36f3103b9f844776f1625ddac073e369a5c4
Daniele Brugnara <daniele@brugnara.me>
2014-07-29 14:59:29 +0200

    12

---
+++ exercises/basics/12.0.js
@@ -0,0 +1,25 @@
+function rnd() {
+  return ~~(Math.random() * 256);
+}
+
+function rndIp() {
+  return rnd() + '.' + rnd() + '.' + rnd() + '.' + rnd();
+}
+
+function getRandomStr() {
+  switch (rnd() % 5) {
+  case 0: return 'ERROR: Device %s shutted down abnormally';
+  case 1: return 'WARN: Device sent a corrupted package. IP: %s';
+  case 2: return 'INFO: Got good news from %s. It is working fine now.';
+  case 3: return 'LOG: Nothing wrong today!';
+  case 4: return 'DANGER: Device (%s) is not reachable since 4 days!';
+  }
+}
+
+function writer() {
+  for (var i=0;i<500;i++) {
+    console.log(getRandomStr(), rndIp());
+  }
+}
+
+setInterval(writer, 3000);
---
+++ exercises/basics/12.1.js
@@ -0,0 +1,10 @@
+// Recupera quanto possibile dall'esercizio 11.1 modularizzando.
+
+// In questo esercizio, dovrai restare in ascolto di modifiche al file di log, ed eseguire le operazioni
+// dell'esercizio 11.1. Accadrà che il file verrà scritto a blocchi ogni tot. Il tuo programma dovrà essere in grado di digerire il file man mano che arrivano dati.
+
+// lancia il generatore di log così:
+
+// node 12.0.js > file.out
+
+Puoi fermarlo quando vorrai con CTRL+C
